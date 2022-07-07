4c9652aea842732f17bb0af6d8edf6e01bd85b94
New-Anointing Aremu <aremunewanointing@gmail.com>
2022-07-07 14:26:54 +0100

    added save funtionallity for Purchase summary and Purchase Report

---
+++ Utility/ReportObj.cs
@@ -0,0 +1,11 @@
+﻿namespace Stock_keeping.Utility
+{
+    public class ReportObj
+    {
+        public string PurchaseDate { get; set; }
+
+        public int SupplierId { get; set; }
+
+        public double TotalCost { get; set; }
+    }
+}
