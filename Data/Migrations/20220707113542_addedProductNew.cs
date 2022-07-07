bfb6692f2c2df80d7a952c62a3d8c9f521cf4208
New-Anointing Aremu <aremunewanointing@gmail.com>
2022-07-07 12:52:35 +0100

    commiting new mkodle update

---
+++ Data/Migrations/20220707113542_addedProductNew.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Stock_keeping.Data.Migrations
+{
+    public partial class addedProductNew : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProductNew",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    SellingPrice = table.Column<double>(type: "float", nullable: false),
+                    CostPrice = table.Column<double>(type: "float", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OrgId = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductNew", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductNew_Category_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Category",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductNew_CategoryId",
+                table: "ProductNew",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductNew");
+        }
+    }
+}
---
+++ Models/PurchaseReport.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Stock_keeping.Models
+{
+    public class PurchaseReport
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime PurchaseTime { get; set; }
+
+        [Required]
+        public int SupplierId { get; set; }
+        [ForeignKey("SupplierId")]
+        public virtual Supplier Supplier { get; set; }
+
+
+        [Required]
+        public int ProductId { get; set; }
+
+        [ForeignKey("ProductId")]
+        public virtual Product Product { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public double TotalCost { get; set; }
+        public string OrgId { get; set; }
+
+
+    }
+}
